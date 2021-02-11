d238e7b3ce6fc60d23d404678d4b9943643c9d8f
Ana C Sanchez <ana@anacsanchez.com>
2021-02-10 19:45:53 -0500

    added import for main stylesheet into storybook preview, changed spacing, removed unnecessary babelrc in storybook folder

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,5 +1,6 @@
 import { addDecorator } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
 import '@storybook/addon-console';
+import '../public/style.css';
 
 addDecorator((storyFn, context) => withConsole()(storyFn)(context));
