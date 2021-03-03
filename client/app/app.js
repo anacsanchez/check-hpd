db88505b08247b7bf97c91c14f59624a010aedef
Ana Sanchez <asanchez@undertone.com>
2021-03-02 22:10:16 -0500

    added react query to app

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,17 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import { Home } from './components';
+import { QueryClient, QueryClientProvider} from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const App = () => {
 	return (
-		<Home />
+		<QueryClientProvider client={queryClient}>
+			<Home />
+			<ReactQueryDevtools initialIsOpen />
+		</QueryClientProvider>
 	);
 };
 
