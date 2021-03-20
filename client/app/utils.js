c5e06a7e7c70b7197cef271ad8c572574e8d5b9b
Ana Sanchez <asanchez@undertone.com>
2021-03-19 22:09:33 -0400

    added format date function to client utils

--- client/app/utils.js
+++ client/app/utils.js
@@ -4,3 +4,8 @@ export const isValidAddress = (input) => {
     }
     return !!input.match(/[\w\d-]+\s[\w\d].*/);
 };
+
+export const formatDate = (dateStr) => {
+    const dateObj = new Date(dateStr);
+    return dateObj.toLocaleDateString(undefined, {year: 'numeric', month: 'numeric', day: 'numeric'});
+};
